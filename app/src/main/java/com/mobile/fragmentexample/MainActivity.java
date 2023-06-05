fae4ac6ce22378b3f1cd18f9af91614e518bef17
estebanparrarodriguez <esteban.parrarodriguez@belmont.edu>
2023-06-05 11:01:38 -0500

    Completed FragmentExample

---
+++ app/src/main/java/com/mobile/fragmentexample/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mobile.fragmentexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/mobile/fragmentexample/PetViewModel.java
@@ -0,0 +1,14 @@
+package com.mobile.fragmentexample;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.mobile.fragmentexample.model.Pet;
+
+import java.util.LinkedList;
+
+public class PetViewModel extends ViewModel {
+
+
+}
